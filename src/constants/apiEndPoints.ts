0fa2e681f66a9c96a10cd477d977d14d05e84772
Monank <134866667+monankp1@users.noreply.github.com>
2025-04-29 10:04:39 +0530

    Update apiEndPoints.ts

--- src/constants/apiEndPoints.ts
+++ src/constants/apiEndPoints.ts
@@ -1,5 +1,5 @@
 const apiURL = {
-    BACKEND_ENDPOINT: 'https://3.111.30.151:3333'
+    BACKEND_ENDPOINT: 'http://3.111.30.151:3333'
     //BACKEND_ENDPOINT: 'https://shibir-backend-api.onrender.com'
 }
 
